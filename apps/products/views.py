b54c88723bbd84969e6e57ba5c7b24ab39da670e
manulangat1 <emmanuelthedeveloper@gmail.com>
2024-04-30 12:31:25 +0300

    feat-implements-products-categories-functuinalities
    Adds models and endpoints for the products and categories functionalities

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+import django_filters
+import logging
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters, generics, permissions, status
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .exceptions import ProductNotFound, CategoryNotFound
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
+from .pagination import ProductPagination
+
+
+logger = logging.getLogger(__name__)
+
+
+class ProductFilter(django_filters.FilterSet):
+    price = django_filters.NumberFilter()
+    price__gt = django_filters.NumberFilter(field_name="price", lookup_expr="gt")
+    price__lt = django_filters.NumberFilter(field_name="price", lookup_expr="lt")
+
+    class Meta:
+        model = Product
+        fields = ["price"]
+
+
+class ListAllProductAPIView(generics.ListAPIView):
+    #  this allows for even unathenticated users to browse the products
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all().order_by("-created_at")
+    pagination_class = ProductPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_class = ProductFilter
+    search_fields = ["name"]
+    ordering_fields = ["created_at"]
+
+
+# class CategoryListView(generics.ListCreateAPIView):
