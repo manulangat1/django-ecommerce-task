a0316b0609d4ce39b57ae68fd3885daebcc26374
manulangat1 <emmanuelthedeveloper@gmail.com>
2024-05-13 14:18:35 +0300

    Implements order functionality

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ListAllProductAPIView.as_view(), name="list_all_products"),
-    path("<str:id>/", views.GetProductDetail.as_view(), name="get_product_detail"),
     path("create/", views.CreateProductApiView.as_view(), name="create_product"),
+    path("<str:id>/", views.GetProductDetail.as_view(), name="get_product_detail"),
 ]
